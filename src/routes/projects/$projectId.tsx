99ccfe17918b4a617d4ab1daf28fcf0a8147bfec
Patrick Lin <>
2026-01-18 19:20:15 +0100

    feat: update project layout with ProjectSidebar

--- src/routes/projects/$projectId.tsx
+++ src/routes/projects/$projectId.tsx
@@ -1,19 +1,16 @@
 import { createFileRoute, Outlet } from '@tanstack/react-router';
 
+import { ProjectSidebar } from '@/components/project-sidebar';
+
 export const Route = createFileRoute('/projects/$projectId')({
   component: ProjectLayout,
 });
 
 function ProjectLayout() {
-  const { projectId } = Route.useParams();
-
   return (
     <div className="flex h-full">
-      {/* Project Sidebar will go here */}
-      <div className="w-64 border-r border-neutral-700 p-4">
-        <h2 className="font-semibold">Project: {projectId}</h2>
-      </div>
-      <div className="flex-1">
+      <ProjectSidebar />
+      <div className="flex-1 overflow-auto">
         <Outlet />
       </div>
     </div>
